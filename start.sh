785264f7c0575340c47aade531629f3ad0b28101
Yusuf Talha ARABACI <yusuftalhaarabaci@hotmail.com>
2024-07-08 11:10:01 +0300

    start scripts is completed

---
+++ start.sh
@@ -0,0 +1,43 @@
+cd API1 
+npm install
+node app.js
+
+cd ../API2
+npm install
+node app.js
+
+cd ../API3
+npm install
+node app.js
+
+cd ../API4
+npm install
+node app.js
+
+cd ../API5
+npm install
+node app.js
+
+cd ../API6
+npm install
+node app.js
+
+cd ../API7
+npm install
+node app.js
+
+cd ../API8
+npm install
+node app.js
+
+cd ../API9
+npm install
+node app.js
+
+cd ../API10
+npm install
+node app.js
+
+cd ../web
+npm install
+node app.js
