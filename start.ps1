09edc622a5280d5dc150dc55832d8f5f4c2e90d7
Yusuf Talha ARABACI <77548038+yusufarbc@users.noreply.github.com>
2024-07-18 18:27:32 +0300

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -2,42 +2,42 @@ cd API1
 npm install
 node app.js
 
-cd ../API2
+cd ..\API2
 npm install
 node app.js
 
-cd ../API3
+cd ..\API3
 npm install
 node app.js
 
-cd ../API4
+cd ..\API4
 npm install
 node app.js
 
-cd ../API5
+cd ..\API5
 npm install
 node app.js
 
-cd ../API6
+cd ..\API6
 npm install
 node app.js
 
-cd ../API7
+cd ..\API7
 npm install
 node app.js
 
-cd ../API8
+cd ..\API8
 npm install
 node app.js
 
-cd ../API9
+cd ..\API9
 npm install
 node app.js
 
-cd ../API10
+cd ..\API10
 npm install
 node app.js
 
-cd ../web
+cd ..\web
 npm install
 node app.js
